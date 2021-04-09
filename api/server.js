c3b0c668aaa6dea822912f655e191c50ef887abf
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-09 08:25:07 -0400

    index and server are coonected and listening on 5000

--- api/server.js
+++ api/server.js
@@ -1,5 +1,18 @@
+const { json } = require('express');
 const express = require('express');
 const server = express();
+
+const actionRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router")
+
+server.use(express,json());
+
+//server.use("/api/actions", actionRouter);
+//server.use("api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+    res.json({message: "it is connecting"})
+})
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
